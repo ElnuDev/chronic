e5d1ad57c11575851f547ad86410923a836265e8
ElnuDev <elnu@elnu.com>
2021-09-02 10:16:00 -0700

    Work on setup code for import from habitctl

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use const_format::concatcp;
 use std::{
     fs::File,
-    io::{BufRead, BufReader},
+    io::{stdin, BufRead, BufReader},
     path::Path,
 };
 use uuid::Uuid;
@@ -10,13 +10,51 @@ const HABITCTL_DIR: &str = "/home/elnu/.habitctl";
 const HABITCTL_HABITS: &str = concatcp!(HABITCTL_DIR, "/habits");
 const HABITCTL_LOG: &str = concatcp!(HABITCTL_DIR, "/log");
 
+const DIR: &str = "/home/elnu/.chronic";
+const HABITS: &str = concatcp!(DIR, "/habits");
+const LOG: &str = concatcp!(DIR, "/log");
+
+const NAME: &str = "chronic";
+
 fn main() {
+    if !installed() {
+        setup();
+    }
     if habitctl_installed() {
         println!("habitctl is installed.");
         parse_habitctl_data();
     } else {
         println!("habitctl isn't installed.");
     }
+}
+
+fn setup() {
+    println!("Welcome to {}!", NAME);
+    if habitctl_installed() {
+        println!(
+            "A habitctl installation has been detected in {}",
+            HABITCTL_DIR
+        );
+        println!("Would you like to import it into {}? (Y/n)", NAME);
+        let import = loop {
+            let mut input = String::new();
+            stdin().read_line(&mut input).unwrap();
+            input = input.trim().to_lowercase();
+            if input.is_empty() || input == "y" {
+                break true;
+            } else if input == "n" {
+                break false;
+            }
+            println!("Invalid response.");
+        };
+        if import {
+            parse_habitctl_data();
+        }
+    }
+}
+
+fn installed() -> bool {
+    Path::new(HABITS).is_file() && Path::new(LOG).is_file()
 }
 
 fn habitctl_installed() -> bool {
